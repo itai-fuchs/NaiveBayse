ac3d710e8c31da990ea64f9e36f56e989cccd91a
Itai fuchs <itaifuchs@gmail.com>
2025-07-23 15:05:50 +0300

    23/07/25 add time stamp ERROR

--- APP/main.py
+++ APP/main.py
@@ -13,6 +13,7 @@ def setup_logging():
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
         handlers=[
             logging.FileHandler("C:/Users/itai/PycharmProjects/Navie_Bayse/LOGS/app.log"),
         ]
